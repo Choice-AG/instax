1846b8575d78f1fb4fd8f9ef7777363a95fbf6ee
Goizane <goizaneolaneta@gmail.com>
2024-05-26 18:18:13 +0200

    Remove unused Android files and update Firebase dependencies

--- android/app/src/main/kotlin/com/example/instax/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.instax
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- packages/user_repository/lib/src/firebase_user_repository.dart
+++ packages/user_repository/lib/src/firebase_user_repository.dart
@@ -1,12 +1,86 @@
-import 'user_repo.dart';
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:user_repository/user_repository.dart';
 
 class FirebaseUserRepository implements UserRepository {
-  // Sign in
+  FirebaseUserRepository({
+    FirebaseAuth? firebaseAuth,
+  }) : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
 
-  // Sign out
+  final FirebaseAuth _firebaseAuth;
+  final usersCollection = FirebaseFirestore.instance.collection('users');
 
-  // Sign up
+  @override
+  Stream<User?> get user {
+    return _firebaseAuth.authStateChanges().map((firebaseUser) {
+      final user = firebaseUser;
+      return user;
+    });
+  }
 
-  // Reset Password
-  
+  @override
+  Future<MyUser> signUp(MyUser myUser, String password) async {
+    try {
+      UserCredential user = await _firebaseAuth.createUserWithEmailAndPassword(email: myUser.email, password: password);
+
+      myUser = myUser.copyWith(id: user.user!.uid);
+
+      return myUser;
+    } catch (e) {
+      log(double.parse(e.toString()));
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> singIn(String email, String password) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+    } catch (e) {
+      log(double.parse(e.toString()));
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> logOut() async {
+    try {
+      await _firebaseAuth.signOut();
+    } catch (e) {
+      log(double.parse(e.toString()));
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> resetPassword(String email) {
+    try {
+      return _firebaseAuth.sendPasswordResetEmail(email: email);
+    } catch (e) {
+      log(double.parse(e.toString()));
+      rethrow;
+    }
+  }
+
+  @override
+  Future<MyUser> getMyUser(String myUserId) {
+    try {
+      return usersCollection.doc(myUserId).get().then((doc) => MyUser.fromEntity(MyUserEntity.fromDocument(doc.data()!)));
+    } catch (e) {
+      log(double.parse(e.toString()));
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> setUserData(MyUser user) async {
+    try {
+      await usersCollection.doc(user.id).set(user.toEntity().toDocument());
+    } catch (e) {
+      log(double.parse(e.toString()));
+      rethrow;
+    }
+  }
 }
